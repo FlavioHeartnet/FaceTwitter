7971d444da6d92129909890ebfea7be176c2e664
flavio <flavionogueirabarros@gmail.com>
2015-09-23 22:08:54 -0300

    layouts prontos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,50 +1,94 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Face Twitter</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        
         <style>
-            html, body {
-                height: 100%;
-            }
-
+            
             body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
+            padding-top: 70px; /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
             }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
+            .portfolio-item {
+            margin-bottom: 25px;
             }
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
+        footer {
+        margin: 50px 0;
+        }
+            
         </style>
+        
     </head>
     <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
-            <a href="#" class="btn btn-primary">Botão</a>
-            <div class="content">
-                <div class="title">Aplicação</div>
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Bem vindo ao FaceTwitter</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Perfil</a>
+                    </li>
+                    <li>
+                        <a href="#">Configurações</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+        
+        <div class="container">
+            <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Seu Perfil
+                    <small>Veja sua timeline do Facebook e twitter</small>
+                </h1>
+            </div>
+        </div>
+            
+            <div class="row">
+                <div class="content col-lg-10">
+                <a class="twitter-timeline" href="https://twitter.com/Flavio_Heartnet" data-widget-id="646836160447254528">Tweets de @Flavio_Heartnet</a>  
+                
+            </div>
+                
             </div>
             
+          
         </div>
         
-        <a class="twitter-timeline" href="https://twitter.com/Flavio_Heartnet" data-widget-id="646836160447254528">Tweets de @Flavio_Heartnet</a>
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Flavio Nogueira website 2015</p>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+        
+        
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
     </body>
 </html>
